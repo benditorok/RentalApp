303d4f38366cc0081fd8a164b169ea5d4b138d5d
Török Bendegúz <32034793+benditorok@users.noreply.github.com>
2023-12-22 17:30:59 +0100

    Added generic repository

---
+++ RentalApp.Repository/Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalApp.Repository.Repository;
+
+/// <summary>
+/// Interface for genereic CRUD functions.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IRepository<T> where T : class
+{
+    void Create(T item);
+
+    T Read(int id);
+
+    void Update(T item);
+
+    void Delete(int id);
+
+    IQueryable<T> ReadAll();
+}
---
+++ RentalApp.Repository/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using RentalApp.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalApp.Repository.Repository;
+
+/// <summary>
+/// Generic abstract repository implementing CRUD functions.
+/// </summary>
+public abstract class Repository<T> : IRepository<T> where T : class
+{
+    private RentalAppDbContext ctx;
+
+    public Repository(RentalAppDbContext ctx)
+    {
+        this.ctx = ctx;
+    }
+
+    public void Create(T item)
+    {
+        ctx.Set<T>().Add(item);
+        ctx.SaveChanges();
+    }
+
+    public abstract T Read(int id);
+
+    public abstract void Update(T item);
+
+    public void Delete(int id)
+    {
+        ctx.Set<T>().Remove(Read(id));
+        ctx.SaveChanges();
+    }
+
+    public IQueryable<T> ReadAll()
+    {
+        return ctx.Set<T>();
+    }
+}
