1d0aff6648b7333f7ba3101312e1c1dff1809bbe
Török Bendegúz <32034793+benditorok@users.noreply.github.com>
2023-12-22 17:01:32 +0100

    Basic DbContext structure

--- RentalApp.Repository/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RentalApp.Repository
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ RentalApp.Repository/Context/RentalAppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalApp.Repository.Context;
+
+public class RentalAppDbContext : DbContext
+{
+    public DbSet<Car> Cars { get; set; }
+
+    public DbSet<Customer> Customers { get; set; }
+
+    public DbSet<Rental> Rentals { get; set; }
+
+    public DbSet<Maintenance> Maintenances { get; set;}
+
+    public RentalAppDbContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
