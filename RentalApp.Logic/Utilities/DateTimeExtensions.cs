521a4489d2e71e30d201621ab2b353df219bacc2
Török Bendegúz <32034793+benditorok@users.noreply.github.com>
2024-01-17 20:41:47 +0100

    Updated logic library

---
+++ RentalApp.Logic/Utilities/DateTimeExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace RentalApp.Logic.Utilities;
+
+public static class DateTimeExtensions
+{
+    public static DateTime? SpecifyKindUTC(this DateTime? dateTime)
+    {
+        if (dateTime.HasValue)
+            return dateTime.Value.SpecifyKindUTC();
+
+        return null;
+    }
+
+    /// <summary>
+    /// Returns the value converted to UTC if necessary.
+    /// </summary>
+    /// <param name="dateTime">DateTime</param>
+    /// <returns></returns>
+    public static DateTime SpecifyKindUTC(this DateTime dateTime)
+    {
+        if (dateTime.Kind == DateTimeKind.Utc) 
+            return dateTime;
+        
+        return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+    }
+
+    public static void SetKindUTC(this ref DateTime? dateTime)
+    {
+        if (dateTime.HasValue)
+            dateTime.Value.SpecifyKindUTC();
+    }
+
+    /// <summary>
+    /// Converts the value to UTC if necessary.
+    /// </summary>
+    /// <param name="dateTime">DateTime</param>
+    /// <returns></returns>
+    public static void SetKindUTC(this ref DateTime dateTime)
+    {
+        dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+    }
+}
